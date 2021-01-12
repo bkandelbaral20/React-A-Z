ee898857d2e06088de2241d6ba23a0a06cbed55a
Binjita <binjita.baral@gmail.com>
2021-01-11 22:02:13 -0600

    expressions and conditionals

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,20 @@ import './App.css';
 
 class App extends Component {
   render() {
+    //creating  variable
+    const name = 'Binjita';
+    //creating function
+    const foo = () => 'Bar';
+    //conditionals
+    const loading = true;
+
+    if(loading){
+      return <h4>Loading...</h4>;
+    }
     return (
         <div className="App">
-          <h1>Hello from React</h1>
+          <h1>Hello from React to {name.toUpperCase()}</h1>
+          <h1>Hello {foo()} </h1>
         </div>
     );
   }
