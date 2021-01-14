ef09fc8d32740c3249ac7fcb8fef3cb913221294
Binjita <binjita.baral@gmail.com>
2021-01-13 22:09:26 -0600

    adding spinner component if page is loading

---
+++ my-app/src/layout/Spinner.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react'
+import loader from "./loader.gif"
+
+//creating spinner function and adding img on it
+const Spinner = () =>
+     <Fragment>
+         <img src={loader} alt={"loading"} style={{width :'200px', margin: 'auto', display:'block'}} />
+     </Fragment>
+
+export default Spinner
